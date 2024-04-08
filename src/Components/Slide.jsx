f6198ba8e40baf9e3569b1b4af7075def81fcb85
sujonifty <sujonifty@gmail.com>
2024-04-08 14:32:59 +0600

    completed  slide section

--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -5,38 +5,69 @@ import img4 from "../assets/4.jpg"
 import img5 from "../assets/5.jpg"
 import img6 from "../assets/6.jpg"
 import img7 from "../assets/7.jpg"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/bundle';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import '../Css/slide.css'
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const Slide = () => {
     return (
-        <Swiper>
-            <SwiperSlide className="">
-                <img src={img1} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img2} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img3} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img4} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img5} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img6} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-            <SwiperSlide className="">
-                <img src={img7} className="w-full bg-cover" alt="" />
-            </SwiperSlide>
-         
-        </Swiper>
+        <>
+            <Swiper
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={img1} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img2} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img3} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img4} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img5} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img6} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={img7} />
+                </SwiperSlide>
+            </Swiper>
+        </>
     );
 };
+
+
 
 export default Slide;
